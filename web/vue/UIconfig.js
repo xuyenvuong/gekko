fb1ca80f3266fa2ebd51cead38f9d1e84b3e0b5c
xuyenvuong <mvuong@livongo.com>
2018-02-24 18:07:58 -0800

    testing

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,16 +4,16 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
   ui: {
-    ssl: false,
-    host: 'localhost',
-    port: 3000,
+    ssl: true,
+    host: '73.222.234.123',
+    port: 4443,
     path: '/'
   },
   adapter: 'sqlite'
