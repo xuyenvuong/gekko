23ce1341c57f6edbc869aad7440d264e7593487e
xuyenvuong <mvuong@livongo.com>
2018-02-27 11:07:48 -0800

    test

--- strategies/MV_Combo.js
+++ strategies/MV_Combo.js
@@ -131,7 +131,7 @@ method.check = function(candle) {
 
     if(this.trend.persisted && !this.trend.adviced) {
       if (macd.signal.result < -1.0 && macd.signal.result > -2.0) {
-        if (macd.signal.result <= -1.5) {
+        if (macd.signal.result >= -1.5) {
           if (emaDiff > this.lastData.emaDiff) {
             log.debug('  >>>>>>>>>>>>>>>>>>>>>>>> BUY BUY BUY MACD -1.0', candle.close.toFixed(d));
           }
