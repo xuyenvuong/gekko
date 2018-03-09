0f6c623caea5596849c9d7e5a54f3ef4ed75145a
xuyenvuong <mvuong@livongo.com>
2018-03-08 17:55:05 -0800

    test

--- strategies/MV_Combo2.js
+++ strategies/MV_Combo2.js
@@ -235,14 +235,14 @@ method.check = function(candle) {
 
     if (cs.isBullish(b)) {
       if (this.trend.duration < 3 && p > (this.trend.duration * 0.1) + 0.3) { // TODO: adjust const
-        this.setTrend('short', 0);
+        this.setTrend('short', 1);
         log.debug('  <<<<<<<<<<<<<<<<<<<<<<<< SELL SELL SELL by Spike #1', candle.close.toFixed(d), 'pl:', this.pl += candle.close);
       } else if (p >= 0.5) {               // TODO: param or AI about this
         this.setTrendSignal({on: cs.isBearish, do: 'confirm'}, {on: cs.isBearish, do: 'short', keep: 2}, {wait: 1, do: 'hold'});
       }
     } else if (cs.isBearish(b)) {
       if (this.trend.duration < 3 && p > (this.trend.duration * 0.1) + 0.3) { // TODO: adjust const
-        this.setTrend('long', 0);
+        this.setTrend('long', 1);
         log.debug('  >>>>>>>>>>>>>>>>>>>>>>>> BUY BUY BUY Spike #1', candle.close.toFixed(d), 'pl:', this.pl -= candle.close);
       } else {
         if (p >= 0.5) {               // TODO: param or AI about this
