3cec562b7274ff5bb5b70465ccdaaf9c6215876c
xuyenvuong <mvuong@livongo.com>
2018-03-10 15:29:13 -0800

    test

--- strategies/MV_Combo2.js
+++ strategies/MV_Combo2.js
@@ -190,15 +190,15 @@ method.check = function(candle) {
    */
   if (!this.trend.adviced) {
     if (this.trend.lastAdvice == 'long') {
-      if (cs.isBullishHarami(lastCandle, candle) && this.trend.duration > 1) {
+      if (cs.isBullishHarami(lastCandle, candle)) {
         this.setTrend('short', 0);
-        log.debug('  <<<<<<<<<<<<<<<<<<<<<<<< SELL SELL SELL isBullishHarami #2', candle.close.toFixed(d), 'pl:', this.pl += candle.close);
+        log.debug('  <<<<<<<<<<<<<<<<<<<<<<<< SELL SELL SELL isBullishHarami #1', candle.close.toFixed(d), 'pl:', this.pl += candle.close);
       } else if (cs.isBearishHarami(lastCandle, candle)) { //TODO only if lower than short price
         this.setTrend('short', 0);
         log.debug('  <<<<<<<<<<<<<<<<<<<<<<<< SELL SELL SELL isBearishHarami #1', candle.close.toFixed(d), 'pl:', this.pl += candle.close);
       }
     } else if (this.trend.lastAdvice == 'short') {
-      if (cs.isBearishHarami(lastCandle, candle) && this.trend.duration > 1) {
+      if (cs.isBearishHarami(lastCandle, candle)) {
         this.setTrend('long', 0);
         log.debug('  >>>>>>>>>>>>>>>>>>>>>>>> BUY BUY BUY isBearishHarami #1', candle.close.toFixed(d), 'pl:', this.pl -= candle.close);
       } else if (cs.isBullishHarami(lastCandle, candle)) { // TODO only if lower than short price
